eb5a5c0f384b4c16637f793918509ffc116a8a08
poprsp <poprsp@zoho.com>
2018-11-18 07:56:41 +0000

    Load weighted scores on submit

--- ui/app.js
+++ ui/app.js
@@ -8,8 +8,44 @@ function addFormListener() {
   document.getElementById("form").addEventListener("submit", e => {
     const userList = document.getElementById("user-list");
     const user = userList[userList.selectedIndex].value;
-    console.log(`user: ${user}`);
+
+    loadWeightedScores(user);
   });
+}
+
+function loadWeightedScores(user) {
+  const url = getUrl("weighted-scores");
+  const result = document.getElementById("result");
+
+  /* Remove all children except for the header */
+  while (result.children.length > 1) {
+    result.removeChild(result.children[1]);
+  }
+
+  fetch(`${url}/${user}`)
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      for (const item of json) {
+        const row = document.createElement("tr");
+
+        const movie = document.createElement("td");
+        const movieText = document.createTextNode(item.movie);
+        movie.appendChild(movieText);
+
+        const score = document.createElement("td");
+        const scoreText = document.createTextNode(item.score.toFixed(4));
+        score.appendChild(scoreText);
+
+        row.appendChild(movie);
+        row.appendChild(score);
+        result.appendChild(row);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function loadUserList() {
